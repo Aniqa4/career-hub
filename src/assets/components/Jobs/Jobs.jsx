9bf55feab4e408dd9d5a386c52f7d151d5afa7b7
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-04-12 16:53:01 +0600

    added see all jobs

--- src/assets/components/Jobs/Jobs.jsx
+++ src/assets/components/Jobs/Jobs.jsx
@@ -3,21 +3,17 @@ import { Link } from 'react-router-dom';
 
 function Jobs() {
     const [jobs,setJobs]=useState([]);
+    const [showAll,setShowAll]=useState(false);
+
     useEffect( () =>
     {
         fetch('json/jobs.json')
         .then(res => res.json())
-        .then(data => showFew(data))
+        .then(data => setJobs(data))
     },[]);
 
-    const showFew=(x)=>{
-        setJobs(x.slice(0,4));
-    }
-    
-    
-    const showAll= ()=>
-    {
-        console.log('clicked');
+    const handleShowAll=()=>{
+        setShowAll(!showAll)
     }
 
   return (
@@ -26,7 +22,39 @@ function Jobs() {
         <p className=' text-center pb-5'>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
         <div className='grid grid-cols-2 gap-5'>
             {  
-                jobs.map( job => 
+                jobs.slice(0,4).map( job => 
+                    
+                        <div key={job.id} className='border rounded-md p-5'>
+                            <img src={job.companyLogo} style={{width:'100px'}} />
+                            <h1 className=' font-bold'>
+                                {job.jobTitle}
+                            </h1>
+                            <h1>
+                                {job.companyName}
+                            </h1>
+                            <div className=' flex gap-5 py-2'>
+                                <button className='rounded border border-purple-500 p-2 text-purple-500'>
+                                    {job.remoteOrOnsite}
+                                </button>
+                                <button className='rounded border border-purple-500 p-2 text-purple-500'>
+                                    {job.fulltimeOrParttime}
+                                </button>
+                            </div>
+                            <div className='flex gap-5'>
+                                <p>
+                                    {job.location}
+                                </p>
+                                <p>
+                                   Salary : {job.salary}
+                                </p>
+                            </div>
+                            <button className='text-white font-semibold bg-purple-500 rounded py-2 px-4 my-2'>
+                                <Link to={`/details/${job.id}`} > View Details</Link></button>
+                        </div>
+                    )
+            }
+            {  
+               showAll&& jobs.slice(4).map( job => 
                     
                         <div key={job.id} className='border rounded-md p-5'>
                             <img src={job.companyLogo} style={{width:'100px'}} />
@@ -59,7 +87,8 @@ function Jobs() {
             }
         </div>
         <div className=' text-center p-5'>
-           <button onClick={() => showAll()} className='text-white font-semibold bg-purple-500 rounded py-2 px-4 my-2'>See All Jobs</button>
+           <button onClick={() => handleShowAll()} className='text-white font-semibold bg-purple-500 rounded py-2 px-4 my-2'>
+            {showAll?'Hide Jobs':'Show All Jobs'}</button>
         </div>
     </div>
   )
